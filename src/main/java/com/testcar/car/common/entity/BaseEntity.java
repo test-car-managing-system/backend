4ef0c8303a53d42e780e2061f0ece3aece677d39
gengminy <kls1998@naver.com>
2023-11-29 17:25:27 +0900

    prod: jpa 및 기본 엔티티 설정

---
+++ src/main/java/com/testcar/car/common/entity/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.testcar.car.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Getter
+@MappedSuperclass
+public class BaseEntity {
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(nullable = false)
+    protected Boolean deleted = false;
+
+    public void delete() {
+        this.deleted = true;
+    }
+}
