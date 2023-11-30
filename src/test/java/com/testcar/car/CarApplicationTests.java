24d457cd647dc1e8b66952a14391ba50328aa920
gengminy <kls1998@naver.com>
2023-11-30 16:51:14 +0900

    prod: docker ci/cd 구성

--- src/test/java/com/testcar/car/CarApplicationTests.java
+++ src/test/java/com/testcar/car/CarApplicationTests.java
@@ -1,12 +1,7 @@
 package com.testcar.car;
 
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CarApplicationTests {
-
-    @Test
-    void contextLoads() {}
-}
+class CarApplicationTests {}
