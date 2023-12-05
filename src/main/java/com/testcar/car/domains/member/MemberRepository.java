94a9d37b575651ec24639e7897807090d9c8231c
gengminy <kls1998@naver.com>
2023-12-05 11:50:09 +0900

    feat: querydsl 설정

--- src/main/java/com/testcar/car/domains/member/MemberRepository.java
+++ src/main/java/com/testcar/car/domains/member/MemberRepository.java
@@ -1,9 +1,10 @@
 package com.testcar.car.domains.member;
 
 
+import com.testcar.car.domains.member.repository.MemberCustomRepository;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberCustomRepository {
     Optional<Member> findByIdAndDeletedFalse(Long id);
 }
